00ec501dc29a9ec08630285bc6e4d517b7e313b2
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-02-17 07:41:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,14 +45,9 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://t.me/+-ykEAVSxL1VjZTdl'),
-            InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/+lZITR_An_DhiYmY1')
-            ],[
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
-        ]]
+            InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ]]
+            
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -91,14 +86,9 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://t.me/+-ykEAVSxL1VjZTdl'),
-            InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/+lZITR_An_DhiYmY1')
-            ],[
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
-        ]]
+            InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ]]
+            
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
